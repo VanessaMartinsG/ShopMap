830643c9b519caa1bd152676117b6dfe8867eb8e
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-06-21 23:41:37 -0300

    Feat: login + register

--- prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
+++ prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
@@ -2,9 +2,14 @@ package com.example.prototipo_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private ImageView image_backPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,18 @@ public class FormCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes2();
+
+        image_backPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(FormCadastro.this,formLogin.class);
+               startActivity(intent);
+            }
+        });
+    }
+
+   private void IniciarComponentes2(){
+       image_backPage = findViewById(R.id.backPage);
     }
 }
