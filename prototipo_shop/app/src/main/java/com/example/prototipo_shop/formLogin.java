30c42dd7da0ba4479bb1a00d4d9c1887e2978473
jadsonnobre245 <jadsonnobre098@gmail.com>
2022-06-21 22:25:39 -0300

    Atualização de login

--- prototipo_shop/app/src/main/java/com/example/prototipo_shop/formLogin.java
+++ prototipo_shop/app/src/main/java/com/example/prototipo_shop/formLogin.java
@@ -2,13 +2,32 @@ package com.example.prototipo_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class formLogin extends AppCompatActivity {
+
+    private TextView text_Tcadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        IniciarComp();
+        text_Tcadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(formLogin.this,FormCadastro.class);
+                startActivity(intent);
+            }
+        });
+    }
+    private void IniciarComp(){
+        text_Tcadastro = findViewById(R.id.textCadastro);
     }
 }
