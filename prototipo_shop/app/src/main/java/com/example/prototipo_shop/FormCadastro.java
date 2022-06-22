fd816e450747743429b312500fe52f77364954d8
VanessaMartinsG <82179516+VanessaMartinsG@users.noreply.github.com>
2022-06-21 22:57:04 -0300

    Feat: Add Registration

--- prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
+++ prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
@@ -10,5 +10,7 @@ public class FormCadastro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
     }
 }
