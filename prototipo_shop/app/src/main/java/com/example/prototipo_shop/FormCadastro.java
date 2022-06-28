d2829f415f5f5a21f4c655ab255e817198277aee
jadsonnobre245 <jadsonnobre098@gmail.com>
2022-06-27 23:59:45 -0300

    Firebase Connection
    Conexão com o banco porem, não esta funcional, só esta habilitado

--- prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
+++ prototipo_shop/app/src/main/java/com/example/prototipo_shop/FormCadastro.java
@@ -2,13 +2,38 @@ package com.example.prototipo_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private ImageView image_backPage;
+
+    private EditText edit_nome, edit_email, edit_senha, edit_dataNascimento, edit_senhaconfirmar;
+    private Button bt_cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+        IniciarComponentes2();
+
+        image_backPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent intent = new Intent(FormCadastro.this,formLogin.class);
+               startActivity(intent);
+            }
+        });
+    }
+
+   private void IniciarComponentes2(){
+       image_backPage = findViewById(R.id.backPage);
     }
 }
---
+++ prototipo_shop/app/src/main/java/com/example/prototipo_shop/PagPrincipal.java
@@ -0,0 +1,14 @@
+package com.example.prototipo_shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pag_principal);
+    }
+}
